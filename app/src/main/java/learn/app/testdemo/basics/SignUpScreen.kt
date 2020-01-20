b700d8989990d8870fad6b256fd9f8dbbc29d9d2
008AjayRawat <ajayrawat.android@gmail.com>
2020-01-20 15:07:00 +0530

    Finished forget password

---
+++ app/src/main/java/learn/app/testdemo/basics/SignUpScreen.kt
@@ -0,0 +1,6 @@
+package learn.app.testdemo.basics
+
+//Added signup screen feature branch
+//Resuming signup screen feature branch
+//Finished signup screen feature branch
+//Forget Password Screen Done.
--- app/src/main/java/learn/app/testdemo/basics/TestFile.kt
+++
@@ -1,3 +0,0 @@
-package learn.app.testdemo.basics
-
-//First feature branches by using git flow.....
